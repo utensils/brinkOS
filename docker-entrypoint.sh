988ca9259628913a860726f9811b633494fe7b3a
James Brink <brink.james@gmail.com>
2018-04-30 00:45:42 -0700

    Added logo.png and upated entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,15 +9,6 @@ keep_alive(){
 }
 
 if [ "$#" -eq 0 ]; then
-	echo "Building brinkOS installer and deps"
-	cd /AUR/packages/qt5-styleplugins-git
-	sudo -u build makepkg -si --noconfirm
-	repo-add /AUR/repo/AUR.db.tar.gz *.pkg.tar.xz
-    mv *.pkg.tar.xz /AUR/repo/
-    cd /build/packages/brinkOS-installer
-    sudo -u build makepkg -s --noconfirm
-    repo-add /build/brinkOS-packages/brinkOS.db.tar.gz *.pkg.tar.xz
-    mv *.pkg.tar.xz /build/brinkOS-packages/
 	echo "Building brinkOS"
 	cd /build/archlive/ || exit
 	./build.sh -v -N brinkOS -o /iso &
